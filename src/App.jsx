a091444c5688560b4953e0b24bedf196a785bd7e
Kevin Peset <kevin.peset@gmail.com>
2024-06-26 14:41:41 +0200

    ajout search bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
-  const [coffee, setCoffee] = useState([]);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-  const fetchData = () => {
+  const handleChangeSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchData = () => {
+    // www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+    // Nous devons utiliser cette route API en modifiant Arrabiata
+
     axios
-      .get("https://random-data-api.com/api/coffee/random_coffee")
-      .then((response) => setCoffee(response.data))
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+      .then((response) => setData(response.data.meals))
       .catch((error) => console.error(error));
   };
 
-  console.info(coffee);
+  console.log(data);
 
   return (
     <>
       <h1>Fetch</h1>
-      <button onClick={fetchData}>Fetch DATA</button>
+
+      <input
+        onChange={handleChangeSearchInput}
+        type="text"
+        name=""
+        id=""
+        placeholder="Cherche une recette"
+      />
+      <button onClick={searchData}>RECHERCHER</button>
+      <ul>
+        {data === null
+          ? "Aucun résultat"
+          : data.map((plat) => <li key={plat.idMeal}>{plat.strMeal}</li>)}
+      </ul>
     </>
   );
 }
